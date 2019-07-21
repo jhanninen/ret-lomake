1288846428f9e497eb249ba83bcaf64fe03df59d
Juho Hänninen <juho.hanninen@aalto.fi>
2019-07-21 12:06:39 +0300

    Move navigation buttons to the sticky menu.

--- src/components/Kulukorvaus.jsx
+++ src/components/Kulukorvaus.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Info from "./kulukorvaus/Info";
 import Lomake from "./kulukorvaus/Lomake";
 import Esikatselu from "./kulukorvaus/Esikatselu";
-import { Button, Header } from "semantic-ui-react";
+import { Button, Header, Grid, GridRow, Menu } from "semantic-ui-react";
 
 class Kulukorvaus extends Component {
   constructor(props) {
@@ -28,14 +28,32 @@ class Kulukorvaus extends Component {
   render() {
     return (
       <div>
-        <Header as="h1">Kulukorvauslomake</Header>
-        {this.state.page == 1 && <Info />}
-        {this.state.page == 2 && <Lomake />}
-        {this.state.page == 3 && <Esikatselu />}
-        <Button.Group>
-          <Button labelPosition='left' icon='left chevron' onClick={e => this.prevPage()} content='Edellinen' />
-          <Button labelPosition='right' icon='right chevron' onClick={e => this.nextPage()} content='Seuraava' />
-        </Button.Group>
+        <Grid columns={1} style={{ marginBottom: "5em" }}>
+          <Grid.Row>
+            <Grid.Column textAlign="center">
+              <Header as="h1">Kulukorvauslomake</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              {this.state.page == 1 && <Info />}
+              {this.state.page == 2 && <Lomake />}
+              {this.state.page == 3 && <Esikatselu />}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Menu fixed="bottom">
+          <Grid columns={1} verticalAlign="middle" container >
+            <Grid.Column textAlign="center">
+              <GridRow>
+              <Button.Group >
+                <Button labelPosition='left' icon='left chevron' onClick={e => this.prevPage()} content='Edellinen' />
+                <Button labelPosition='right' icon='right chevron' onClick={e => this.nextPage()} content='Seuraava' />
+              </Button.Group>
+              </GridRow>
+            </Grid.Column>
+          </Grid>
+        </Menu>
       </div>
     );
   }
