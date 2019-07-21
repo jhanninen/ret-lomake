e066d43ecb5c1956d86bf36c4f326801d6dd4529
Juho Hänninen <juho.hanninen@aalto.fi>
2019-07-21 12:51:48 +0300

    Create smarter way to handle subpages

--- src/components/Kulukorvaus.jsx
+++ src/components/Kulukorvaus.jsx
@@ -3,14 +3,28 @@ import React, { Component } from "react";
 import Info from "./kulukorvaus/Info";
 import Lomake from "./kulukorvaus/Lomake";
 import Esikatselu from "./kulukorvaus/Esikatselu";
-import { Button, Header, Grid, GridRow, Menu } from "semantic-ui-react";
+import { Button, Header, Grid, Menu, Step } from "semantic-ui-react";
 
 class Kulukorvaus extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 1
-    }
+      page: 0
+    };
+    this.pages = [
+      {
+        name: "Info",
+        component: <Info />
+      },
+      {
+        name: "Lomake",
+        component: <Lomake />
+      },
+      {
+        name: "Esikatselu",
+        component: <Esikatselu />
+      }
+    ];
   }
 
   nextPage() {
@@ -28,7 +42,7 @@ class Kulukorvaus extends Component {
   render() {
     return (
       <div>
-        <Grid columns={1} style={{ marginBottom: "5em" }}>
+        <Grid columns={1} style={{ marginTop: "1em", marginBottom: "6em" }}>
           <Grid.Row>
             <Grid.Column textAlign="center">
               <Header as="h1">Kulukorvauslomake</Header>
@@ -36,21 +50,41 @@ class Kulukorvaus extends Component {
           </Grid.Row>
           <Grid.Row>
             <Grid.Column>
-              {this.state.page == 1 && <Info />}
-              {this.state.page == 2 && <Lomake />}
-              {this.state.page == 3 && <Esikatselu />}
+              {this.pages[this.state.page].component}
             </Grid.Column>
           </Grid.Row>
         </Grid>
-        <Menu fixed="bottom">
-          <Grid columns={1} verticalAlign="middle" container >
-            <Grid.Column textAlign="center">
-              <GridRow>
-              <Button.Group >
-                <Button labelPosition='left' icon='left chevron' onClick={e => this.prevPage()} content='Edellinen' />
-                <Button labelPosition='right' icon='right chevron' onClick={e => this.nextPage()} content='Seuraava' />
+        <Menu fixed="bottom" size="tiny">
+          <Grid columns={1} verticalAlign="middle" container textAlign="center">
+            <Grid.Column style={{paddingBottom: "1px"}}>
+              <Grid.Row>
+                <Step.Group unstackable>
+                  {this.pages.map((item, i) => (
+                    <Step key={i}
+                      content={item.name}
+                      active={this.state.page === i}
+                      disabled={this.state.page < i}
+                      onClick={e => this.setState({ page: i })}
+                    />
+                  ))}
+                </Step.Group>
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Column style={{paddingTop: "1px"}}>
+              <Grid.Row>
+              <Button.Group size="tiny">
+                <Button labelPosition='left' icon='left chevron'
+                  onClick={e => this.prevPage()}
+                  disabled={this.state.page === 0}
+                  content='Edellinen'
+                />
+                <Button labelPosition='right' icon='right chevron'
+                  onClick={e => this.nextPage()}
+                  disabled={this.state.page === this.pages.length - 1}
+                  content='Seuraava'
+                />
               </Button.Group>
-              </GridRow>
+              </Grid.Row>
             </Grid.Column>
           </Grid>
         </Menu>
