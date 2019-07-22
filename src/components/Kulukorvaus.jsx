5be962ac65d26b907296d64b0f51484da43541dc
Juho Hänninen <juho.hanninen@aalto.fi>
2019-07-22 12:05:14 +0300

    Move navigation away from menu to get better mobile compability.

--- src/components/Kulukorvaus.jsx
+++ src/components/Kulukorvaus.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Info from "./kulukorvaus/Info";
 import Lomake from "./kulukorvaus/Lomake";
 import Esikatselu from "./kulukorvaus/Esikatselu";
-import { Button, Header, Grid, Menu, Step } from "semantic-ui-react";
+import { Button, Header, Grid, Step } from "semantic-ui-react";
 
 class Kulukorvaus extends Component {
   constructor(props) {
@@ -42,37 +42,36 @@ class Kulukorvaus extends Component {
   render() {
     return (
       <div>
-        <Grid columns={1} style={{ marginTop: "1em", marginBottom: "6em" }}>
+        <Grid columns={1} style={{ marginTop: "1em", marginBottom: "4em" }}>
           <Grid.Row>
             <Grid.Column textAlign="center">
               <Header as="h1">Kulukorvauslomake</Header>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column>
-              {this.pages[this.state.page].component}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        <Menu fixed="bottom" size="tiny">
-          <Grid columns={1} verticalAlign="middle" container textAlign="center">
-            <Grid.Column style={{paddingBottom: "1px"}}>
-              <Grid.Row>
+              <Grid.Column textAlign="center">
                 <Step.Group unstackable>
                   {this.pages.map((item, i) => (
                     <Step key={i}
                       content={item.name}
                       active={this.state.page === i}
                       disabled={this.state.page < i}
-                      onClick={()=> this.setState({ page: i })}
                     />
                   ))}
                 </Step.Group>
-              </Grid.Row>
+              </Grid.Column>
+            </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              {this.pages[this.state.page].component}
             </Grid.Column>
-            <Grid.Column style={{paddingTop: "1px"}}>
-              <Grid.Row>
-              <Button.Group size="tiny">
+          </Grid.Row>
+        </Grid>
+        <Grid columns={1}
+          container textAlign="center">
+          <Grid.Row>
+            <Grid.Column>
+              <Button.Group size="tiny" fixed="bottom">
                 <Button labelPosition='left' icon='left chevron'
                   onClick={() => this.prevPage()}
                   disabled={this.state.page === 0}
@@ -84,10 +83,9 @@ class Kulukorvaus extends Component {
                   content='Seuraava'
                 />
               </Button.Group>
-              </Grid.Row>
             </Grid.Column>
-          </Grid>
-        </Menu>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
